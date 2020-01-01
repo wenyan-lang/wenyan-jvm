fbf5b70eef3546f74c4afe6efb3789369aa9819f
magiclu550 <843983728@qq.com>
2020-01-01 21:37:32 +0800

    添加标准库

---
+++ src/main/java/cn/wenyan/lang/算经.java
@@ -0,0 +1,15 @@
+package cn.wenyan.lang;
+
+/**
+ * 此为文言文算数之书，亦可明正弦余弦之义
+ */
+public class 算经 {
+
+    public static double 正弦(double x){
+        return Math.sin(x);
+    }
+
+    public static double 余弦(double x){
+        return Math.cos(x);
+    }
+}

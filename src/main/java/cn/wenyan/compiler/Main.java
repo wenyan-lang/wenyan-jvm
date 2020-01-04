c99093eabaa483f8ecf3b850ad0811e2774b55cd
magiclu550 <843983728@qq.com>
2020-01-04 21:25:30 +0800

    加入'之'

--- src/main/java/cn/wenyan/compiler/Main.java
+++ src/main/java/cn/wenyan/compiler/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) throws Exception{
         WenYanCompiler compiler = new WenYanCompilerImpl(false);
         compiler.runDirectly(true,
-                "吾嘗觀「「java之lang之Math」」之書。方悟「pow」之義。" +
+                "吾嘗觀「「java之lang之Math」」之書。" +
                         "有數五十。名之曰「大衍」。" +
                         "吾有一術。名之曰「翻倍」。欲行是術。必先得一數。曰「甲」。乃行是術曰。" +
                         "   乘「甲」以二。名之曰「乙」。乃得「乙」。" +
                         "是謂「翻倍」之術也。" +
-                        "施「pow」於「大衍」於一。名之曰「矣」。書之");
+                        "施「Math之pow」於「大衍」於二。名之曰「矣」。書之");
 
     }
 }

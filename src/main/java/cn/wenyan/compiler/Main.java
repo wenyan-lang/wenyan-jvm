6301cc7f0cb9339e27188c97fc1a3105276f10cb
magiclu550 <843983728@qq.com>
2020-01-11 13:35:43 +0800

    支持強類型

--- src/main/java/cn/wenyan/compiler/Main.java
+++ src/main/java/cn/wenyan/compiler/Main.java
@@ -13,9 +13,5 @@ public class Main {
         }
         WenYanCompiler compiler = new WenYanCompilerImpl(false, Language.GROOVY);
         compiler.compile(args);
-        WenYanCompiler wenYanCompiler = WenYanTools.makeCompiler(Language.GROOVY);
-        wenYanCompiler.runDirectly(true,"" +
-                "吾有一術。名之曰「六脈神劍」。欲行是術。必先得六數。曰「甲」。曰「乙」。曰「丙」。曰「丁」。曰「戊」。曰「己」乃行是術曰。是謂「六脈神劍」之術也。" +
-                "");
     }
 }

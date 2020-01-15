492afefd66896a919061bc35781d6c11c051bbc8
magiclu550 <843983728@qq.com>
2020-01-15 21:15:30 +0800

    Main

--- src/main/java/cn/wenyan/compiler/Main.java
+++ src/main/java/cn/wenyan/compiler/Main.java
@@ -16,14 +16,13 @@ public class Main {
             CommandHandler.compileCommand.entrySet().stream().forEach(x->System.out.println(x.getValue().getOption()+": "+x.getValue().getClass().getSimpleName()));
         }
         long start = System.currentTimeMillis();
-        WenYanCompiler compiler = new WenYanCompilerImpl(false, Language.GROOVY);
+        WenYanCompilerImpl compiler = new WenYanCompilerImpl(false, Language.GROOVY);
 
         compiler.compile(args);
 
-        System.out.println(compiler.dispatch("夫「甲」。夫「乙」。夫「丙」。取二以施「丁」。取二以施「戊」。取一以施「己」。夫「庚」。夫「辛」。取三以施「壬」。名之曰「癸」。"));
         long end = System.currentTimeMillis();
 
-        System.out.println("Use: "+(end-start)+"ms");
+        compiler.getServerLogger().debug("Use: "+(end-start)+"ms");
 
     }
 }

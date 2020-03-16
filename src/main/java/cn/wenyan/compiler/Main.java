72dca133eeec0801dc4b279d929e8296a69ccaa0
magiclu550 <843983728@qq.com>
2020-03-16 10:41:43 +0800

    you can use run to run groovy code

--- src/main/java/cn/wenyan/compiler/Main.java
+++ src/main/java/cn/wenyan/compiler/Main.java
@@ -20,30 +20,31 @@ import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args){
-        if(args.length == 1 && args[0].equals("shell")){
+    public static void main(String[] args) {
+        if (args.length == 1 && args[0].equals("shell")) {
             WenYanShell.run();
             return;
         }
-        if(args.length == 2 && args[0].equals("run")) {
+        if (args.length == 2 && args[0].equals("run")) {
             try {
                 Class<?> runner = new GroovyClassLoader(Main.class.getClassLoader()).parseClass(new File(args[1]));
-                runner.getDeclaredMethod("main",String[].class).invoke(null,(Object)args);
-            }catch (Exception e){
+                runner.getDeclaredMethod("main", String[].class).invoke(null, (Object) args);
+            } catch (Exception e) {
                 e.printStackTrace();
-            return;
-        }
+                return;
+            }
 
-        if (args.length == 0||args[0].equals("help")){
-            CommandHandler.compileCommand.entrySet().stream().forEach(x->System.out.println(x.getValue().getOption()+": "+x.getValue().help()));
-            return;
-        }
-        WenYanCompilerImpl compiler = new WenYanCompilerImpl(false, Language.GROOVY);
-        long time = ScalaUtils.countTime(()->{
-            compiler.compile(args);
-            return null;
-        });
-        compiler.getServerLogger().debug("Use: "+time+"ms");
+            if (args.length == 0 || args[0].equals("help")) {
+                CommandHandler.compileCommand.entrySet().stream().forEach(x -> System.out.println(x.getValue().getOption() + ": " + x.getValue().help()));
+                return;
+            }
+            WenYanCompilerImpl compiler = new WenYanCompilerImpl(false, Language.GROOVY);
+            long time = ScalaUtils.countTime(() -> {
+                compiler.compile(args);
+                return null;
+            });
+            compiler.getServerLogger().debug("Use: " + time + "ms");
 
+        }
     }
 }
